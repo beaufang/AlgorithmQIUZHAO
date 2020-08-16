2b2263c2c25b33d508945560bdaa87e1ad672e8d
beaufang <beaufang@126.com>
2020-08-16 10:45:50 +0800

    week5

--- Week_05/WordSearchII_2.java
+++ Week_05/WordSearchII_2.java
@@ -1,18 +1,16 @@
-# Week5 学习总结
+package com.beau.leetcode.week5;
 
-## LeetCode 练习
 
-![image-20200816103211517](../resources/image-20200816103211517.png)
+import org.junit.Test;
 
-[Excel 源文件](../leetcode刷题2020.xlsx)
+import java.util.ArrayList;
+import java.util.List;
 
-# WordSearchII 复杂度分析
-
-```java
 /**
  * @author BeauFang
  * Date: 2020/8/15
  * 212 https://leetcode-cn.com/problems/word-search-ii/
+ * 该解法直接在遍历的过程中修改 Trie
  */
 public class WordSearchII_2 {
 
@@ -83,54 +81,14 @@ public class WordSearchII_2 {
 
         }
     }
-}
 
-```
+    @Test
+    public void test() {
+        System.out.println(findWords(new char[][]{{'a', 'a'}}, new String[]{"aa"}));
+        System.out.println(findWords(new char[][]{{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}},
+                new String[]{"oath","pea","eat","rain"}));
 
-假设 borad 中字母的个数为 M 个，每个字母都需要进行一次上下左右四个方向的扩展，在第二次扩展的时候，除掉原方向，需要扩展三个方向。假设单词的长度为 L，则最坏情形下的复杂度为 `O(m * 4 * 3^(L-1))`
-
-# 双向BFS 模板
-
-双向 BFS 中，使用的 Queue 要改为 Set，方便快速判断两个集合是否右交集。每次从较小的一段开始遍历。通常在知道终点的情况下使用。
-
-```java
-public int binBfs(Node start, Node target) {
-    Set<Node> q1 = new HashSet<>();
-    Set<Node> q2 = new HashSet<>();
-    Set<Node> visited = new HashSet<>();
-    q1.add(start);
-    q2.add(target);
-    visited.add(start);
-    int step = 0;
-    while (!q1.isEmpty() && !q2.isEmpty()) {
-        // 总是从较小的一端处理
-        if (q1.size() > q2.size()) {
-            Set<Node> tmp = q1;
-            q1 = q2;
-            q2 = tmp;
-        }
-        // 用于存储下一层的节点
-        Set<Node> nextLevel = new HashSet<>();
-        for (Node cur : q1) {
-            // 两端相交
-            if (q2.contains(cur)) {
-                return  step;
-            }
-            if (cur.children == null) {
-                continue;
-            }
-            for (Node child : cur.children) {
-                if (!visited.contains(child)) {
-                    nextLevel.add(child);
-                }
-            }
-        }
-        step++;
-        // 注意是直接覆盖
-        q1 = nextLevel;
-        visited.addAll(nextLevel);
+        System.out.println(findWords(new char[][]{{'a','a'}},
+                new String[]{"aaa"}));
     }
-    return -1;
 }
-```
-
